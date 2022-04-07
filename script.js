9d04c289b3a5a8874e66ff1a47f818043ddb1e7c
myroslav <myrkozar@gmail.com>
2022-04-08 00:30:13 +0300

    test-den
    done

--- script.js
+++ script.js
@@ -1,88 +1,5 @@
 'use strict'
 
-const refsMainBlock = document.querySelector('.container')
-const refsTableBlock = document.querySelector('.test__task')
-const refsTableCell = document.querySelector('.test__table__data')
-const refsTableCellText = document.querySelectorAll('.text__content')
-const refsTableCellTextEl = document.querySelector('.text__content')
-const refsArrowUp = document.querySelector('.up')
-const refsArrowUpArr = document.querySelectorAll('.up')
-const refsArrowDown = document.querySelector('.down')
-const refsArrowDownArr = document.querySelectorAll('.down')
-const refsInput = document.querySelector('[type="checkbox"]')
-const refsBtn = document.querySelectorAll('.btn')
-//console.log(refsTableCellText[1].lastChild.textContent);
-
-function addAddEventListenerl (arr, arr1) {
-    arr.forEach(element => element.addEventListener('click', onArrowDownClick))
-    arr1.forEach(element => element.addEventListener('click', onArrowUpClick))
-}
-// addAddEventListenerl(refsArrowDownArr, refsArrowUpArr)
-
-function onArrowDownClick (event) {
-    
-const text = event.currentTarget.parentNode.lastChild.textContent 
-event.currentTarget.parentNode.lastChild.textContent = event.currentTarget.parentNode.parentNode.nextElementSibling.firstChild.lastChild.textContent
-event.currentTarget.parentNode.parentNode.nextElementSibling.firstChild.lastChild.textContent = text
-
-}
-
-function onArrowUpClick (event) {
-   
-    const text = event.currentTarget.parentNode.lastChild.textContent 
-    event.currentTarget.parentNode.lastChild.textContent = event.currentTarget.parentNode.parentNode.previousElementSibling.firstChild.lastChild.textContent
-    event.currentTarget.parentNode.parentNode.previousElementSibling.firstChild.lastChild.textContent = text
-    
-    }
-
-// refsBtn[0].addEventListener('click', onBtnClickForHidden)
-
-// function onBtnClickForHidden (event) {
-//     console.log();
-//     event.currentTarget.parentNode.firstElementChild.classList.toggle('hidden')
-//     if(event.currentTarget.parentNode.firstElementChild.classList.contains('hidden')){
-//          event.currentTarget.textContent = 'показать таблицу'
-//     }else{
-//         event.currentTarget.textContent = 'спрятать таблицу'
-//     }
-    
-// }
-
-refsBtn[1].addEventListener('click', onBtnClickForCorectText)
-
-function onBtnClickForCorectText (event) {
-
-    makeUnderlineBelowText(refsTableCellText)
-    createNewText()
-}
- function makeUnderlineBelowText (arr) {
-    arr.forEach(element => {
-        if(element.classList.contains('underline') === false){
-           element.classList.add('underline')
-        }else{
-            element.classList.remove('underline')
-        }
-    })
- }
-// lastChild.textContent
-refsTableCellTextEl.addEventListener('input', createNewText)
-// console.log(refsTableCellTextEl);
-//  function changeText (arr) {
-//     arr.forEach(element => {element.addEventListener('input', createNewText)
-//     })
-//  }
-  
- function createNewText (event) {
-    //  console.log(refsTableCellTextEl);
-    //  console.log(event)
-    //  console.dir(refsTableCellTextEl.lastChild.textContent);
-    
- }
- createNewText()
-//  makeUnderlineBelowText(refsTableCellText)
-//{element.lastChild.textContent = element.lastChild.textContent.value}
-//  element.lastChild.textContent = 'ghjdfghj'
-
 
 const refsLastElDiv = document.querySelector('.new')
 const refsLastElDivArr = refsLastElDiv.querySelectorAll('.js-lastDivCell')
@@ -103,38 +20,31 @@ function addEventListenerToArrowsUp (arrUp, arrDown) {
         arrDown.forEach(arrowDown => 
             arrowDown.addEventListener('click', onArrowDownClicToReversText)
     )
-
-
 }
 addEventListenerToArrowsUp (refsArrowUp1, refsArrowDown1)
 
 function onArrowUpClicToReversText(event){
-    console.log();
+
     console.log(event.currentTarget.parentNode.parentNode.previousElementSibling.querySelector('.input').value);
     const currentValue = event.currentTarget.parentNode.lastChild.value
    const siblingLeftValue = event.currentTarget.parentNode.parentNode.previousElementSibling.querySelector('.input')
    event.currentTarget.parentNode.lastChild.value = siblingLeftValue.value
    event.currentTarget.parentNode.parentNode.previousElementSibling.querySelector('.input').value = currentValue
 
-
-
 }
 
 function onArrowDownClicToReversText(event){
  
-
     const nextVelue = event.currentTarget.parentNode.parentNode.nextElementSibling.querySelector('.input')
     const currentValue = event.currentTarget.parentNode.lastChild.value
     event.currentTarget.parentNode.lastChild.value = nextVelue.value
     event.currentTarget.parentNode.parentNode.nextElementSibling.querySelector('.input').value = currentValue
-
 
 }
 
 const refsButtonBlock = document.querySelectorAll('.btn1')
 
 refsButtonBlock[0].addEventListener('click', onBtnClickForHiddenTable)
-console.log();
 
 function onBtnClickForHiddenTable (event) {
     refsLastElDiv.classList.toggle('hidden')
@@ -143,50 +53,45 @@ function onBtnClickForHiddenTable (event) {
     }else{
         event.currentTarget.textContent = 'спрятать таблицу'
     }
-    
 }
 
 refsButtonBlock[1].addEventListener('click', makeUnderlineBelowTextInput)
-console.log(refsLastInputArr);
 
-function makeUnderlineBelowTextInput (arr) {
-    // arr.forEach(element => {
-    //     if(element.classList.contains('underline') === false){
-    //        element.classList.add('underline')
-    //     }else{
-    //         element.classList.remove('underline')
-    //     }
-    // })
+function makeUnderlineBelowTextInput (event) {
+
+    refsLastInputArr.forEach(element => 
+        element.classList.toggle('underline') 
+    )
+    refsLastInputArr.forEach(element => 
+       element.hasAttribute('disabled') === true ? element.removeAttribute('disabled') : element.setAttribute('disabled', '' )
+       ) 
+       if(event.currentTarget.textContent === 'дать доступ к ячейке'){
+        event.currentTarget.textContent = 'снять доступ с ячейки'
+       }else{
+        event.currentTarget.textContent = 'дать доступ к ячейке'
+       }
  }
- makeUnderlineBelowTextInput(refsLastInputArr)
 
-// event.currentTarget.parentNode.lastChild.value
-// refsLastInput.addEventListenerl('focus', event =>{
-//     console.log(event.currentTarget);
-//     event.currentTarget.style.outline = 'none'
-// })
+ const refsCheckBoxArr = document.querySelectorAll('[type="checkbox"]')
+ const refsCheckBox = document.querySelector('[type="checkbox"]')
+
+function addAddIventListenerToChackBox (arrChackBox) {
+    console.log(arrChackBox );
+    arrChackBox.forEach(el => el.addEventListener('change', onChangeCheckBoxToGetRefsOnElForRemoveCall))
+}
+addAddIventListenerToChackBox(refsCheckBoxArr)
+
+function onChangeCheckBoxToGetRefsOnElForRemoveCall (event) {
+   console.log(); 
+    const refsElToRem = event.currentTarget.parentNode.parentNode
+    refsButtonBlock[2].addEventListener('click', () => refsElToRem.remove())
+}
 
 
 
-const students = ["Вика", "Андрей", "Олег", "Юля", "Борис", "Катя"];
 
-// function rotate (arr) {
-//     // let el = 
-//      [arr[0], arr[1]] = [arr[1], arr[0]]
 
-//     console.log(arr);
-// }
 
-// rotate(students)
-//  students.sort((a, b) => students.indexOf(b) - students.indexOf(a))
-// console.log(students.indexOf("Вика"));
-//console.log(students);
-
-// const inAlphabetOrder = [...students].sort((a, b) => a.localeCompare(b));
-// console.log(inAlphabetOrder); // ['Андрей', 'Борис', 'Вика', 'Катя', 'Олег', 'Юля']
-
-// const inReversedOrder = [...students].sort((a, b) => b.localeCompare(a));
-// console.log(inReversedOrder); // ['Юля', 'Олег', 'Катя', 'Вика', 'Борис', 'Андрей']
 
 
 
